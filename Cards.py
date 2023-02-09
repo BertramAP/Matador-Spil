c11e57b491049fc82ed7a8141f82914487316284
hackerman69420threefiddy <46356970+hackerman69420threefiddy@users.noreply.github.com>
2023-02-09 12:16:31 +0100

    Kort er done hurra.

--- Cards.py
+++ Cards.py
@@ -164,27 +164,18 @@ def get_coord(i: int):
 def generate_spaces(bot_batch, top_batch):
     chance = Chance()
     parkingplace = parkingSpace()
-    Spaces = [[start()], [street("Rødovrevej", [0, 0, 255], 1200, [50, 250, 750, 2250, 4000, 6000]), street("Hvidovrevej", [0, 0, 255], 1200, [50, 250, 750, 2250, 4000, 6000])], [chance], [payTaxSpace(True)], [shippingPort("Scandlines, Helsingør-Helsingborg")],
-            [street("Roskildevej", [255, 100, 127], 2000, [100, 600,  1800, 5400, 8000, 11000]), street("Valby langgade", [255, 100, 127], 2000, [100, 600,  1800, 5400, 8000, 11000]), street("Allegade", [255, 100, 127], 2400, [150, 800,  2000, 6000, 9000, 12000])], [chance], [prison(False)],
-            [street("Frederiksberg Alle", [0, 255, 0], 2800, [200, 1000, 3000, 9000, 12500, 15000]), street("Bulowsvej", [0, 255, 0], 2800, [200, 1000, 3000, 9000, 12500, 15000]), street("G.L. Kongevej", [0, 255, 0], 3200, [250, 1250, 3750, 10000, 14000, 18000])], [Corparation("Tuborg")], [shippingPort("Mols-Linien")],
-            [street("Bernstorttsvej", [128, 128, 128], 3600, [300, 1400, 4000, 11000, 15000, 19000]), street("Hellerupvej", [128, 128, 128], 3600, [300, 1400, 4000, 11000, 15000, 19000]), street("Strandvejen", [128, 128, 128], 4000, [350, 1600, 4400, 12000, 16000, 20000])], [chance], [parkingplace],
-            [street("Trianglen", [255, 51, 51], 4400, [350, 1800, 5000, 14000, 17500, 21000]), street("Østerbrogade", [255, 51, 51], 4400, [350, 1800, 5000, 14000, 17500, 21000]), street("Grønningen", [255, 51, 51], 4800, [400, 2000, 6000, 15000, 18500, 22000])], [chance], [shippingPort("Scandlines Gedse-Rostock")],
-            [street("Bredgade", [0, 0, 0], 5200, [450, 2200, 6600, 16000, 19500, 23000]), street("Kgs. Nytorv", [0, 0, 0], 5200, [450, 2200, 6600, 16000, 19500, 23000]), street("Østergade", [0, 0, 0], 5600, [500, 2400, 7200, 17000, 20500, 24000])], [Corparation("Coca-Cola")], [prison(True)],
-            [street("Amagertorv", [255, 234, 0], 6000, [550, 2600, 7800, 18000, 22000, 25000]), street("Vimmelskaftet ", [255, 234, 0], 6000, [550, 2600, 7800, 18000, 22000, 25000]), street("Nygade",[255, 234, 0], 6400, [600, 3000, 9000, 20000, 24000, 28000])], [chance], [shippingPort("Scandlines, Rødby-Puttgarden")],
-            [Chance],[street("Frederiksberggade", [255, 234, 0], 7000, [700, 3500, 10000, 22000, 26000, 30000]), street("Rådhuspladsen", [255, 234, 0], 8000, [1000, 4000, 12000, 28000, 34000, 40000])],[payTaxSpace(False)]]
-    newSpaces = []
-
-    """for i in range(len(Spaces)):
-        if (len(Spaces[i]) > 1):
-            for x in range(len(Spaces[i])):
-                if (x == 1):
-                    b = i + 1
-                    newSpaces.append(Spaces[b][0])
-                    newSpaces.append(Spaces[i][x])
-                else:
-                    newSpaces.append(Spaces[i][x])
-                i+=1
-        newSpaces.append(Spaces[i][0])"""
+    Spaces = [start(),street('Rødovrevej', [0, 0, 255], 1200, [50, 250, 750, 2250, 4000, 6000]), Chance(), street('Hvidovrevej', [0, 0, 255], 1200, [50, 250, 750, 2250, 4000, 6000]), 
+    payTaxSpace(True), shippingPort('Scandlines, Helsingør-Helsingborg'),street('Roskildevej', [255, 100, 127], 2000, [100, 600, 1800, 5400, 8000, 11000]), 
+    Chance(),street('Valby langgade', [255, 100, 127], 2000, [100, 600, 1800, 5400, 8000, 11000]), street('Allegade', [255, 100, 127], 2400, [150, 800, 2000, 6000, 9000, 12000]), 
+    prison(), street('Frederiksberg Alle', [0, 255, 0], 2800, [200, 1000, 3000, 9000, 12500, 15000]), Corparation('Tuborg'), street('Bulowsvej', [0, 255, 0], 2800, [200, 1000, 3000, 9000, 12500, 15000]), 
+    street('G.L. Kongevej', [0, 255, 0], 3200, [250, 1250, 3750, 10000, 14000, 18000]), shippingPort('Mols-Linien'),street('Bernstorttsvej', [128, 128, 128], 3600, [300, 1400, 4000, 11000, 15000, 19000]), 
+    Chance(), street('Hellerupvej', [128, 128, 128], 3600, [300, 1400, 4000, 11000, 15000, 19000]), street('Strandvejen', [128, 128, 128], 4000, [350, 1600, 4400, 12000, 16000, 20000]), 
+    parkingSpace(), street('Trianglen', [255, 51, 51], 4400, [350, 1800, 5000, 14000, 17500, 21000]), Chance(), street('Østerbrogade', [255, 51, 51], 4400, [350, 1800, 5000, 14000, 17500, 21000]), 
+    street('Grønningen', [255, 51, 51], 4800, [400, 2000, 6000, 15000, 18500, 22000]), shippingPort('Scandlines Gedse-Rostock'), street('Bredgade', [0, 0, 0], 5200, [450, 2200, 6600, 16000, 19500, 23000]), 
+    street('Kgs. Nytorv', [0, 0, 0], 5200, [450, 2200, 6600, 16000, 19500, 23000]),Corparation('Coca-Cola'),street('Østergade', [0, 0, 0], 5600, [500, 2400, 7200, 17000, 20500, 24000]), 
+    prison(), street('Amagertorv', [255, 234, 0], 6000, [550, 2600, 7800, 18000, 22000, 25000]), street('Vimmelskaftet ', [255, 234, 0], 6000, [550, 2600, 7800, 18000, 22000, 25000]), Chance(),
+    street('Nygade', [255, 234, 0], 6400, [600, 3000, 9000, 20000, 24000, 28000]), shippingPort('Scandlines, Rødby-Puttgarden'), Chance(), street('Frederiksberggade', [255, 234, 0], 7000, [700, 3500, 10000, 22000, 26000, 30000]), 
+    payTaxSpace(False), street('Rådhuspladsen', [255, 234, 0], 8000, [1000, 4000, 12000, 28000, 34000, 40000])]
 
     lst = [0]*40
 
